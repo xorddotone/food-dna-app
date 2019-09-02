b5c142e62f88e6f69d468f231b4c6ec40a1e555d
Abdul Sami <sami-abdul@outlook.com>
2019-09-02 15:27:26 +0500

    Add owner email field

--- src/app/Product/Product.component.ts
+++ src/app/Product/Product.component.ts
@@ -112,7 +112,8 @@ export class ProductComponent implements OnInit {
       'productionDate': this.productionDate.value,
       'expirationDate': this.expirationDate.value,
       'foodDna': this.foodDna.value,
-      'owner': this.owner.value
+      'owner': 'resource:com.food.dna.Business#' + this.owner.value,
+      // 'ownerEmail': this.owner.value
     };
 
     this.myForm.setValue({
@@ -158,7 +159,7 @@ export class ProductComponent implements OnInit {
       'productionDate': this.productionDate.value,
       'expirationDate': this.expirationDate.value,
       'foodDna': this.foodDna.value,
-      'owner': this.owner.value
+      'owner': 'resource:com.food.dna.Business#' + this.owner.value
     };
 
     return this.serviceProduct.updateAsset(form.get('referenceNumber').value, this.asset)
